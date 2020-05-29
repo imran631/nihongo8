be8a6f920ccbe26945243264bd879aa8ac917d44
h4ppyy <93immm@naver.com>
2020-05-29 13:46:28 +0900

    auth

--- backend/urls.py
+++ backend/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('regist', views.regist, name='regist'),
+    path('reset', views.reset, name='reset'),
+    path('reset_password', views.reset_password, name='reset_password'),
     path('sample', views.sample, name='sample'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -5,10 +5,34 @@ from django.http import HttpResponse
 def index(request):
 
     context = {}
-    return render(request, 'backend/index.html', context)
+    return render(request, 'backend/index/index.html', context)
+
+
+def login(request):
+
+    context = {}
+    return render(request, 'backend/auth/login.html', context)
+
+
+def regist(request):
+
+    context = {}
+    return render(request, 'backend/auth/regist.html', context)
+
+
+def reset_password(request):
+
+    context = {}
+    return render(request, 'backend/auth/reset_password.html', context)
+
+
+def reset(request):
+
+    context = {}
+    return render(request, 'backend/auth/reset.html', context)
 
 
 def sample(request):
 
     context = {}
-    return render(request, 'backend/sample.html', context)
+    return render(request, 'backend/auth/sample.html', context)
