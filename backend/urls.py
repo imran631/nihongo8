9b4bc2230008ffd862857dee4fe959fb32a1b453
h4ppyy <93immm@naver.com>
2020-05-29 16:23:59 +0900

    community

--- backend/urls.py
+++ backend/urls.py
@@ -4,10 +4,17 @@ from . import views
 
 
 urlpatterns = [
+
     path('', views.index, name='index'),
+
     path('login', views.login, name='login'),
     path('regist', views.regist, name='regist'),
     path('reset', views.reset, name='reset'),
     path('reset_password', views.reset_password, name='reset_password'),
+
     path('sample', views.sample, name='sample'),
+    path('sample/community', views.sample_community, name='sample_community'),
+    path('sample/community/detail', views.sample_community_detail, name='sample_community_detail'),
+    path('sample/community/write', views.sample_community_write, name='sample_community_write'),
+    path('sample/community/modify', views.sample_community_modify, name='sample_community_modify'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -3,36 +3,50 @@ from django.http import HttpResponse
 
 
 def index(request):
-
     context = {}
     return render(request, 'backend/index/index.html', context)
 
 
 def login(request):
-
     context = {}
     return render(request, 'backend/auth/login.html', context)
 
 
 def regist(request):
-
     context = {}
     return render(request, 'backend/auth/regist.html', context)
 
 
 def reset_password(request):
-
     context = {}
     return render(request, 'backend/auth/reset_password.html', context)
 
 
 def reset(request):
-
     context = {}
     return render(request, 'backend/auth/reset.html', context)
 
 
 def sample(request):
-
     context = {}
-    return render(request, 'backend/auth/sample.html', context)
+    return render(request, 'backend/sample/sample.html', context)
+
+
+def sample_community(request):
+    context = {}
+    return render(request, 'backend/sample/community.html', context)
+
+
+def sample_community_detail(request):
+    context = {}
+    return render(request, 'backend/sample/community_detail.html', context)
+
+
+def sample_community_write(request):
+    context = {}
+    return render(request, 'backend/sample/community_write.html', context)
+
+
+def sample_community_modify(request):
+    context = {}
+    return render(request, 'backend/sample/community_modify.html', context)
